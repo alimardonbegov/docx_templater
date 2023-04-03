00213add8bb0c1852aae198c7414f3b0ed903026
Алимардон Бегов <alimardon007@gmail.com>
2023-04-03 18:27:07 +0200

    write own methods for tpl

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'package:http/http.dart';
+import 'package:tpl/tpl.dart';
+
+void main(List<String> args) async {
+  Future<List<int>> getDocBytesFromRemote(String docxUrl) async {
+    try {
+      final Client _client = Client();
+      final Response req = await _client.get(Uri.parse(docxUrl));
+      final List<int> bytes = req.bodyBytes;
+      return bytes;
+    } catch (e) {
+      throw Exception('error downloading remote .docx template file: ' + e.toString());
+    }
+  }
+
+  Future<List<int>> getDocBytesFromLocal(String docPath) async {
+    try {
+      final file = File(docPath);
+      final List<int> bytes = file.readAsBytesSync();
+      return bytes;
+    } catch (e) {
+      throw Exception('error downloading local .docx template file: ' + e.toString());
+    }
+  }
+
+  final Map<String, String> testMap = {
+    "DOC_NUMBER": "111111",
+    "IME_PREZIME": "AAAAAAA",
+  };
+
+  final List<int> bytes = await getDocBytesFromLocal("lib/tpl.docx");
+
+  final tpl = Templater(bytes: bytes, map: testMap);
+
+  //! save bytes and create new file
+  final List<int> generatedBytes = tpl.generateTpl()!;
+  final file = File("lib/generated_tpl.docx");
+  file.writeAsBytesSync(generatedBytes);
+}
---
+++ lib/tpl.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+import 'package:archive/archive_io.dart';
+import 'package:xml/xml.dart';
+
+class Templater {
+  final List<int> bytes;
+  final Map<String, String> map;
+
+  List<String> _mergedFields = [];
+  late Archive _zip;
+  late String _docXml;
+  late int _documentXmlIndex;
+
+  Templater({
+    required this.bytes,
+    required this.map,
+  });
+
+  void _templateParse(String text) {
+    final List<String> fields = [];
+    final RegExp re = RegExp('{{\\w*}}', caseSensitive: true, multiLine: true);
+    final Iterable<Match> matches = re.allMatches(text);
+
+    if (matches.isEmpty) {
+      fields;
+    } else {
+      for (var match in matches) {
+        final int group = match.groupCount;
+        final String field = match.group(group)!;
+        final String firstChunk = field.replaceAll('{{', '').trim(); // remove templating braces
+        final String secChunk = firstChunk.replaceAll('}}', '').trim(); // remove templating braces
+        fields.add(secChunk);
+      }
+    }
+    _mergedFields = fields.toSet().toList();
+  }
+
+  void _getArchiveAndXmlString() {
+    _zip = ZipDecoder().decodeBytes(bytes);
+
+    // ! check encoding the bytes (not working)
+    // final newBytes1 = ZipEncoder().encode(_zip);
+    // print('original bytes');
+    // print(bytes);
+    // print('rewrite bytes');
+    // print(newBytes1);
+
+    _documentXmlIndex = _zip.files.indexWhere((file) => file.name == 'word/document.xml');
+    final ArchiveFile documentXml = _zip.files[_documentXmlIndex];
+    final List<int> content = documentXml.content as List<int>;
+
+    _docXml = String.fromCharCodes(content);
+  }
+
+  void _writeMergeFields() {
+    for (var field in _mergedFields) {
+      if (map.containsKey(field) &&
+          _docXml.contains(
+            RegExp(
+              '{{\\w*}}',
+              caseSensitive: true,
+              multiLine: true,
+            ),
+          )) {
+        _docXml = _docXml.replaceAll(RegExp('{{$field}}'), map[field]!);
+      }
+    }
+  }
+
+  List<String> get mergeFields => _mergedFields; //get template fields
+
+  List<int>? generateTpl() {
+    _getArchiveAndXmlString();
+    _templateParse(_docXml);
+
+    _writeMergeFields();
+
+    final Archive newZip = Archive();
+    for (var file in _zip.files) {
+      if (file.name != 'word/document.xml') {
+        newZip.addFile(file);
+      } else {
+        final List<int> xmlBytes = utf8.encode(_docXml);
+        final ArchiveFile newWordDocumentXml = ArchiveFile('word/document.xml', xmlBytes.length, xmlBytes);
+        newZip.addFile(newWordDocumentXml);
+      }
+    }
+
+    final newBytes = ZipEncoder().encode(newZip);
+    return newBytes;
+  }
+}
